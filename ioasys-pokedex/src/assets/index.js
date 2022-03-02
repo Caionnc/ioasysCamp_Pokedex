6b5a9116053d670adc4ac24b9cf3a2b0521a6481
Caio Carvalho <caio.carvalho@lavid.ufpb.br>
2022-03-01 23:31:03 -0300

    Home page

---
+++ ioasys-pokedex/src/assets/index.js
@@ -0,0 +1,4 @@
+/*Export Icons*/
+export {
+
+};
---
+++ ioasys-pokedex/src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import { Strings } from "./strings";
+
+import "./styles.css";
+
+function Home() {
+  return (
+    <div className="container">
+      <header className="home-header"></header>
+      <p>Teste</p>
+    </div>
+  );
+}
+
+export default Home;
